c9f2618f6ced22fdf179c37b9462ba0bcc7de279
Aparup <rayaparup007@gmail.com>
2026-06-07 00:08:45 +0530

    Valid parentheses problem

---
+++ Stack/ValidParentheses.js
@@ -0,0 +1,20 @@
+const ValidParentheses = (s) => {
+  const stack = [];
+  const map = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (map[char]) {
+      stack.push(char);
+    } else {
+      const last = stack.pop();
+      if (map[last] !== char) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
